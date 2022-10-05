c183b343e6009bd26139bf1064d048ca947b0463
PeeperInTheSpace <lollof94@gmail.com>
2022-10-05 15:01:04 +0200

    Creating components...

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
 
+    <HeaderSection />
+    <MainSection />
+    <FooterSection />
+
   </div>
 </template>
 
 <script>
+import HeaderSection from './components/HeaderSection.vue';
+import MainSection from './components/MainSection.vue';
+import FooterSection from './components/FooterSection.vue';
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    HeaderSection,
+    MainSection,
+    FooterSection
+}
 }
 </script>
 
