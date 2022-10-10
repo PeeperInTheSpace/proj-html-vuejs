5863e8635ef198bc4b95395d2f013cae505e2c8b
PeeperInTheSpace <lollof94@gmail.com>
2022-10-10 17:49:43 +0200

    Main: EverythingTwoSection Completed!

--- src/components/sub-components/EverythingTwoSection.vue
+++ src/components/sub-components/EverythingTwoSection.vue
@@ -1,8 +1,23 @@
 <template>
   <main>
-      <section>
-
-      </section>
+    <section class="container d-flex justify-content-center align-items-center w-100 p-4">
+      <div class="w-50 position-relative">
+        <img src="../../assets/img/home-3-team-image.png" alt="avatars" class="fl-avatars">
+        <img src="../../assets/img/maxcoach-shape-05-150x150.png" alt="circle-dots-150" class="fl-circledots">
+        <img src="../../assets/img/green-cloud.png" alt="green-cloud" class="fl-greencloud">
+        <img src="../../assets/img/maxcoach-shape-07-100x100.png" alt="square-dots-100" class="fl-squaredots">
+        <img src="../../assets/img/maxcoach-shape-01.png" alt="lines" class="fl-lines">
+      </div>
+      <div class="w-50">
+        <div class="fl-paragraph position-relative">
+          <img src="../../assets/img/brown-circle.png" alt="brown-circle" class="fl-browncircle">
+          <h5 class="mb-3">EVERYTHING IN <strong>MAX COACH</strong></h5>
+          <h1 class="mb-3">We're Here To<br><h1 class="fl-highlighted d-inline">Transform </h1>You!</h1>
+          <p class="mb-3">As learners, people can enjoy great companionship from MaxCoach mentors and educators. We can help you develop and grow at your best.</p>
+          <button class="rounded-1 mt-4">Get Free Guidebook</button>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -16,9 +31,93 @@ export default {
 
 @import "../../style/common.scss"; 
 
-  main {
-      // DEBUG
-      // border: $testBorder;
+  section {
+    height: 75vh;
+
+    .fl-paragraph {
+      width: 445px;
+
+      .fl-browncircle {
+        position: absolute;
+        bottom: 400px;
+        left: -50px;
+      }
+
+      h5 {
+        color: $infoText;
+        position: relative;
+      }
+
+      
+      h1 {
+        font-weight: 600;
+
+        .fl-highlighted {
+          font-weight: 400;
+          color: $primary;
+        }
+
+      }
+
+      span {
+        font-size: 15px;
+        font-weight: 600;
+
+        &:hover {
+            cursor: pointer;
+          }
+
+      }
+
+      button {
+      border: none;
+      padding: 10px 20px;
+      background-color: $primary;
+      color: $blank;
+      font-weight: 600;
+
+      &:hover, span:hover{
+        background-color: $blank;
+        color: $primary;
+      }
+
+    }
+
+
+    }
+
+    .fl-avatars {
+      z-index: 5;
+      position: inherit;
+    }
+    .fl-greencloud {
+      position: absolute;
+      top: 50px;
+      right: 400px;
+      z-index: 2;
+    }
+
+    .fl-circledots {
+      position: absolute;
+      top: 150px;
+      left: -70px;
+      z-index: 3;
+    }
+
+    .fl-squaredots {
+      position: absolute;
+      bottom: -50px;
+      left: 300px;
+      z-index: 1;
+    }
+
+    .fl-lines {
+      position: absolute;
+      bottom: -30px;
+      left: -150px;
+      z-index: 1;
+    }
+
   }
 
 </style>
--- src/components/sub-components/TestimonialsSection.vue
+++ src/components/sub-components/TestimonialsSection.vue
@@ -1,7 +1,42 @@
 <template>
-    <main>
-        <section>
-
+    <main class="main">
+        <section class="container d-flex justify-content-center align-items-center w-100">
+            <div class="w-50">
+                <div class="fl-paragraph">
+                <h5 class="mb-3">TESTIMONIALS</h5>
+                <h1 class="mb-3">Why Do People <h1 class="fl-highlighted d-inline">*Hearts*</h1> Us?</h1>
+                <p class="mb-3">Seeking for verbals of our service quality? Find them here. Everything is transparent and straightforward for your sense of jusitifcation.</p>
+                <strong>View all <i class="fa-solid fa-arrow-right-long"></i></strong>
+                </div>
+            </div>
+            <div class="w-50 position-relative">
+                <div class="fl-mainbox shadow rounded-2 position-absolute p-5">
+                    <h6>Professional team of specialists and passionate mentors at reach</h6>
+                    <p>I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.</p>
+                    <div class="d-flex w-100">
+                        <div class="w-25">
+                            <img src="../../assets/img/testimonial-avata-01.jpg" alt="testimonial-01">
+                        </div>
+                        <div class="w-50 pt-4">
+                            <h6>MADLEY PONDOR</h6>
+                            <p>/ IT Specialist</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="fl-transparentboxtop position-absolute shadow p-4">
+                    <h6>The MaxCoach team works really hard to ensure high level of quality</h6>
+                    <p>I am happy with their arrangement of lessons and subjects. They</p>
+                </div>
+                <div class="fl-transparentboxbottom position-absolute shadow p-4 d-flex">
+                    <div class="w-25">
+                        <img src="../../assets/img/testimonial-avata-02.jpg" alt="testimonial-02">
+                    </div>
+                    <div class="w-50 pt-4">
+                        <h6>MINA HOLLACE</h6>
+                        <p>/ Freelancer</p>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 </template>
@@ -16,9 +51,85 @@ export default {
 
 @import "../../style/common.scss"; 
 
-    main {
-        // DEBUG
-        // border: $testBorder;
+    .main {
+        margin-top: 10vh;
+        height: 65vh;
+
+            .fl-paragraph {
+                width: 445px;
+
+            p {
+                font-size: 15px;
+                font-weight: 500;
+
+                &:hover {
+                    cursor: pointer;
+                }
+
+                }
+
+            }
+
+
+        section {
+        height: 55vh;
+
+            h5 {
+                color: $infoText;
+            }
+
+            h1 {
+                font-weight: 600;
+
+                .fl-highlighted {
+                font-weight: 400;
+                color: $primary;
+                }
+
+            }
+
+            .fl-mainbox {
+                height: 300px;
+                width: 700px;
+                z-index: 3;
+                top: -140px;
+                background-color: $blank;
+
+                
+                img {
+                    height: 100px;
+                    width: 100px;
+                    border-radius: 100%;;
+                }
+            }
+
+            .fl-transparentboxtop {
+                height: 150px;
+                width: 580px;
+                z-index: 2;
+                filter: opacity(0.5);
+                top: -240px;
+                right: 20px;
+            }
+
+            .fl-transparentboxbottom {
+                height: 150px;
+                width: 600px;
+                z-index: 2;
+                top: 120px;
+                left: 60px;
+                filter: opacity(0.5);
+
+                img {
+                    height: 100px;
+                    width: 100px;
+                    border-radius: 100%;;
+                }
+
+            }
+
+
+        }
     }
 
 </style>
