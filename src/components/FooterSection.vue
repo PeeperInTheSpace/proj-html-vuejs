247349626dd9abd9e95b93b829a35455e9560108
PeeperInTheSpace <lollof94@gmail.com>
2022-10-10 18:55:17 +0200

    Main Completed! + Footer Completed!

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,6 +1,42 @@
 <template>
   <footer>
-
+    <section class="container"> 
+      <div class="d-flex justify-content-between">
+        <div class="d-flex flex-column">
+          <h5>Address</h5>
+          <p>382 NE 191st St #87394 Miami, FL 33179-3899</p>
+          <p>+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)</p>
+          <p>support@maxcoach.com</p>
+          <div class="d-flex gap-3">
+            <i class="fa-brands fa-twitter fl-icons"></i>
+            <i class="fa-brands fa-facebook fl-icons"></i>
+            <i class="fa-brands fa-instagram fl-icons"></i>
+            <i class="fa-brands fa-linkedin fl-icons"></i>
+          </div>
+        </div>
+        <div class="am-informations d-flex justify-content-between">
+          <div class="d-flex flex-column">
+            <h5>Explore</h5>
+            <p>Start here</p>
+            <p>Blog</p>
+            <p>About us</p>
+          </div>
+          <div class="d-flex flex-column">
+            <h5>&nbsp;</h5>
+            <p>Success story</p>
+            <p>Courses</p>
+            <p>Contact us</p>
+          </div>
+          <div class="d-flex flex-column">
+            <h5>Informations</h5>
+            <p>Membership</p>
+            <p>Purchase guida</p>
+            <p>Privacy policy</p>
+            <p>Terms of services</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </footer>
 </template>
 
@@ -11,5 +47,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@import "../style/common.scss";
+
+
+  footer {
+    background-color: $footerBackground;
+    color: $blank;
+
+    section{
+        padding-bottom: 5rem;
+        padding-top: 5rem;
+      }
+      .am-informations{
+        width: 30rem;
+      }
+
+      p:hover, .fl-icons:hover {
+        cursor: pointer;
+      }
+  }
+
+
 
 </style>
--- src/components/sub-components/NewsletterSection.vue
+++ src/components/sub-components/NewsletterSection.vue
@@ -1,7 +1,18 @@
 <template>
     <main>
-        <section>
-
+        <section class="container d-flex flex-column align-items-center mb-5">
+            <div class="d-flex flex-column align-items-center text-center fl-titles w-100">
+                <h1 class="m-1">Subscribe <h1 class="d-inline-block fl-highlighted">Newsletter</h1></h1>
+                <div class="w-50 mb-3 position-relative">
+                    <img src="../../assets/img/green-circle.png" alt="green-circle" class="position-absolute fl-greencircle">
+                    <img src="../../assets/img/maxcoach-shape-02.png" alt="green-circle" class="position-absolute fl-semicircles">
+                    <p class="mb-3">Enter your email address to register to our newsletter subscription delivered on a regular basis!</p>
+                </div>
+            </div>
+            <div class="input-group mb-3 fl-subscribe">
+                <input type="text" class="form-control" placeholder="Enter your email">
+                <button class="btn btn-outline-secondary fl-subscribebutton p-3" type="button" id="button-addon2">Subscribe</button>
+            </div>
         </section>
     </main>
 </template>
@@ -20,5 +31,45 @@ export default {
         // DEBUG
         // border: $testBorder;
     }
+
+    .fl-titles {
+
+
+        h5 {
+            color: $infoText;
+        }
+
+        
+        h1 {
+            font-weight: 600;
+            margin-bottom: 30px;
+
+            .fl-highlighted {
+            font-weight: 400;
+            color: $primary;
+            }
+
+        }
+
+        .fl-greencircle {
+            right: 900px;
+        }
+
+        .fl-semicircles {
+            left: 900px;
+        }
+
+    }
+
+    .fl-subscribe {
+        width: 500px;
+
+        .fl-subscribebutton {
+            color: $blank;
+            background-color: $primary;
+            font-weight: 600;
+        }
+    }
+
 
 </style>
